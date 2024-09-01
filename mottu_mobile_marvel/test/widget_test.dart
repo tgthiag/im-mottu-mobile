b0b25d9b1c1624b99da8535ab1da477b05824d96
tgthiag <tgthiag@gmail.com>
2024-09-01 19:51:55 -0300

    Implementing tests

--- mottu_mobile_marvel/test/widget_test.dart
+++ mottu_mobile_marvel/test/widget_test.dart
@@ -1,30 +1,22 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:mottu_mobile_marvel/main.dart';
-
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  setUpAll(() async {
+    TestWidgetsFlutterBinding.ensureInitialized();
+    try {
+      await dotenv.load(fileName: ".env");
+    } catch (e) {
+      print('Error loading .env file: $e');
+    }
+  });
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  test('Dotenv loads environment public variable', () {
+    final apiKey = dotenv.env['MARVEL_PUBLIC_KEY'];
+    expect(apiKey, isNotNull);
+  });
+  test('Dotenv loads environment private variables', () {
+    final apiKey = dotenv.env['MARVEL_PRIVATE_KEY'];
+    expect(apiKey, isNotNull);
   });
 }
