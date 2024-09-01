bd58c4445fe7ff3bd72f7220b195bf40dee69589
tgthiag <tgthiag@gmail.com>
2024-09-01 18:23:40 -0300

    Improved items loading

--- mottu_mobile_marvel/lib/screens/home_page.dart
+++ mottu_mobile_marvel/lib/screens/home_page.dart
@@ -1,31 +1,91 @@
 import 'package:flutter/material.dart';
 
+import '../service/marvel_api.dart';
+import 'marvel_character.dart';
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
+
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final TextEditingController _searchController = TextEditingController();
+  final MarvelApiService marvelApiService = MarvelApiService();
+  List<dynamic> _characters = [];
+  bool _isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadInitialCharacters();
+  }
+
+  Future<void> _loadInitialCharacters() async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    await marvelApiService.fetchAndStoreCharacters();
+    setState(() {
+      _characters = marvelApiService.getCharacters() ?? [];
+      _isLoading = false;
+    });
+  }
+
+  Future<void> searchCharacters(String searchText) async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    await marvelApiService.fetchAndStoreCharacters(
+        searchText: searchText.trim());
+    setState(() {
+      _characters = marvelApiService.getCharacters() ?? [];
+      _isLoading = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Hello world',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      appBar: AppBar(
+        title: const Text(
+          'MARVEL',
+          style: TextStyle(
+              color: Colors.white, fontWeight: FontWeight.w900, fontSize: 52),
         ),
+        backgroundColor: Colors.red,
+        centerTitle: true,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => print("Hello world"),
-        tooltip: 'Teste',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              controller: _searchController,
+              decoration: InputDecoration(
+                hintText: 'Search for a character...',
+                prefixIcon: const Icon(Icons.search),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                filled: true,
+                fillColor: Colors.white,
+              ),
+              onSubmitted: (text) {
+                searchCharacters(text);
+              },
+            ),
+          ),
+          _isLoading
+              ? const Center(child: CircularProgressIndicator())
+              : Expanded(
+                  child: MarvelCharacterPage(characters: _characters),
+                ),
+        ],
+      ),
     );
   }
 }
